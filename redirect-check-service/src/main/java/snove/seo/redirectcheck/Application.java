4906e7628b8f1b273c5887c18d28e64183b46507
snovelli <salvatore.novelli@gmail.com>
2016-08-21 15:12:11 +0100

    Modeled as RedirectSpecification message consumer to scale out

---
+++ redirect-check-service/src/main/java/snove/seo/redirectcheck/Application.java
@@ -0,0 +1,23 @@
+package snove.seo.redirectcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import snove.seo.redirectcheck.model.RedirectSpecification;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@EnableBinding(Processor.class)
+class RedirectSpecSource {
+    @StreamListener(Processor.INPUT)
+    public void handle(RedirectSpecification spec) {
+        System.out.println("Received spec: " + spec);
+    }
+}
