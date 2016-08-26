32a435946ee95565122df7bfb603b4455db5c45f
Salvatore Novelli <salvatore.novelli@ingg.com>
2016-08-26 08:40:31 +0100

    No need of a runner here

--- redirect-check-service/src/test/java/snove/seo/redirectcheck/http/HttpGetRequestTest.java
+++ redirect-check-service/src/test/java/snove/seo/redirectcheck/http/HttpGetRequestTest.java
@@ -17,7 +17,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 
-@RunWith(MockitoJUnitRunner.class)
 public class HttpGetRequestTest {
 
     private Server server;
