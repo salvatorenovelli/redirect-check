aa1d913fed4a04d3d10db8fcadf1bc278e1aeae7
Salvatore Novelli <salvatore.novelli@ingg.com>
2016-09-19 11:43:31 +0100

    Processing message

--- redirect-check-service/src/main/java/snove/seo/redirectcheck/Application.java
+++ redirect-check-service/src/main/java/snove/seo/redirectcheck/Application.java
@@ -1,10 +1,14 @@
 package snove.seo.redirectcheck;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
+
+import snove.seo.redirectcheck.domain.RedirectChainAnalyser;
+import snove.seo.redirectcheck.model.RedirectChain;
 import snove.seo.redirectcheck.model.RedirectSpecification;
 
 @SpringBootApplication
@@ -16,8 +20,16 @@ public class Application {
 
 @EnableBinding(Processor.class)
 class RedirectSpecificationProcessor {
+
+    @Autowired
+    RedirectChainAnalyser analyser;
+
+
     @StreamListener(Processor.INPUT)
     public void handle(RedirectSpecification spec) {
+
+        final RedirectChain redirectChain = analyser.analyseRedirectChain(spec.getSourceURI());
+
         System.out.println("Received spec: " + spec);
     }
 }
