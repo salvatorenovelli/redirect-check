0def6ef409b192e8a1da2176693dd85b3b192142
Salvatore Novelli <salvatore.novelli@ingg.com>
2016-09-26 09:10:00 +0100

    Handling messages from bindings

--- redirect-check-service/src/main/java/snove/seo/redirectcheck/Application.java
+++ redirect-check-service/src/main/java/snove/seo/redirectcheck/Application.java
@@ -1,11 +1,13 @@
 package snove.seo.redirectcheck;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.integration.annotation.Transformer;
 
 import snove.seo.redirectcheck.domain.RedirectChainAnalyser;
 import snove.seo.redirectcheck.model.RedirectChain;
@@ -21,13 +23,19 @@ public class Application {
 @EnableBinding(Processor.class)
 class RedirectSpecificationProcessor {
 
+    private static final Logger logger = LoggerFactory.getLogger(RedirectSpecificationProcessor.class);
+
     @Autowired
     RedirectChainAnalyser analyser;
 
 
-    @StreamListener(Processor.INPUT)
-    public void handle(RedirectSpecification spec) {
+    @Transformer(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+    public RedirectChain handle(RedirectSpecification spec) {
+
+        logger.info("Received spec {}", spec);
         final RedirectChain redirectChain = analyser.analyseRedirectChain(spec.getSourceURI());
-        System.out.println("Received spec: " + spec);
+        logger.info("Analysis of {} completed. Result: {}", spec, redirectChain);
+
+        return redirectChain;
     }
 }
