d2daa6f4e86fb1773a8e9e70efd112561927301e
snovelli <salvatore.novelli@gmail.com>
2016-11-20 15:14:47 +0000

    Minor refactoring

--- redirect-check-api/src/test/java/com/github/salvatorenovelli/redirectcheck/http/HttpGetRequestTest.java
+++ redirect-check-api/src/test/java/com/github/salvatorenovelli/redirectcheck/http/HttpGetRequestTest.java
@@ -26,48 +26,6 @@ public class HttpGetRequestTest {
         server.stop();
     }
 
-
-    /**
-     *
-     * Some stupid website, put unicode characters in their target location (without escaping them).
-     *
-     * The response bytes containing the unicode characters will "decoded" into string with the default charset, and returned as a
-     * header parameter. The problem is that the web server might have encoded them in a different charset than ours,
-     * therefore we had to read the charset from the header and transcode the our string in the intendend charset.
-     *
-     *
-     * This test demonstrate the functionality works.
-     *
-     * PS: I used two passes as it made the test easier as we didn't have to assert against a unicode target URL.
-     *
-     */
-    @Test
-    public void weShouldHandleUnicodeCharacters() throws Exception {
-
-       givenAnHttpServer()
-                .with_301_MovedPermanently("/source", LOCATION_WITH_UNICODE_CHARACTERS)
-                .with_301_MovedPermanently(LOCATION_WITH_UNICODE_CHARACTERS, "/destination")
-                .run();
-
-        HttpResponse firstPass = new HttpGetRequest(testUri("/source")).execute();
-        HttpResponse secondPass = new HttpGetRequest(firstPass.getLocation()).execute();
-
-        assertThat(secondPass.getStatusCode(), is(HttpStatus.SC_MOVED_PERMANENTLY));
-        assertThat(secondPass.getLocation(), is(testUri("/destination")));
-
-    }
-
-    @Test
-    public void unicodeRedirectGetUrlEncoded() throws Exception {
-        givenAnHttpServer()
-                .with_301_MovedPermanently("/source", LOCATION_WITH_UNICODE_CHARACTERS)
-                .run();
-
-        HttpResponse request = new HttpGetRequest(testUri("/source")).execute();
-        assertThat(request.getLocation(), is(testUri("/fam%C3%ADlia")));
-
-    }
-
     @Test
     public void usesGetOn301WithRelativeDstPath() throws Exception {
         givenAnHttpServer()
@@ -116,7 +74,6 @@ public class HttpGetRequestTest {
         assertThat(execute.getLocation(), is(testUri("/destination")));
     }
 
-
     @Test
     public void getOn200() throws Exception {
 
@@ -128,6 +85,47 @@ public class HttpGetRequestTest {
 
         assertThat(execute.getHttpStatus(), is(HttpStatus.SC_OK));
         assertThat(execute.getLocation(), is(testUri("/hello")));
+    }
+
+    /**
+     *
+     * Some stupid website, put unicode characters in their target location (without escaping them).
+     *
+     * The response bytes containing the unicode characters will "decoded" into string with the default charset, and returned as a
+     * header parameter. The problem is that the web server might have encoded them in a different charset than ours,
+     * therefore we had to read the charset from the header and transcode the our string in the intendend charset.
+     *
+     *
+     * This test demonstrate the functionality works.
+     *
+     * PS: I used two passes as it made the test easier as we didn't have to assert against a unicode target URL.
+     *
+     */
+    @Test
+    public void weShouldHandleUnicodeCharacters() throws Exception {
+
+        givenAnHttpServer()
+                .with_301_MovedPermanently("/source", LOCATION_WITH_UNICODE_CHARACTERS)
+                .with_301_MovedPermanently(LOCATION_WITH_UNICODE_CHARACTERS, "/destination")
+                .run();
+
+        HttpResponse firstPass = new HttpGetRequest(testUri("/source")).execute();
+        HttpResponse secondPass = new HttpGetRequest(firstPass.getLocation()).execute();
+
+        assertThat(secondPass.getStatusCode(), is(HttpStatus.SC_MOVED_PERMANENTLY));
+        assertThat(secondPass.getLocation(), is(testUri("/destination")));
+
+    }
+
+    @Test
+    public void unicodeRedirectGetUrlEncoded() throws Exception {
+        givenAnHttpServer()
+                .with_301_MovedPermanently("/source", LOCATION_WITH_UNICODE_CHARACTERS)
+                .run();
+
+        HttpResponse request = new HttpGetRequest(testUri("/source")).execute();
+        assertThat(request.getLocation(), is(testUri("/fam%C3%ADlia")));
+
     }
 
     private TestServerScenarioBuilder givenAnHttpServer() {
